c25819e970135f4e7d468516690a8f93101a5336
Diego0103 <A01254073@itesm.mx>
2021-08-24 19:49:44 +0000

    Tipos de Triángulo

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -4,15 +4,15 @@ def main():
     lado2 = int(input("Ingresa la medida del lado 2: "))
     lado3 = int(input("Ingresa la medida del lado 3: "))
     if lado1>0 and lado2>0 and lado3>0:
-            if lado1+lado2>lado3 and lado1+lado3>lado2 and lado2+lado3>lado1:
-                if lado1==lado2 and lado1==lado3:
-                    print("ES UN TRIANGULO EQUILATERO")
-                elif lado1==lado2 or lado1==lado3 or lado2==lado3:
-                    print("ES UN TRIANGULO ISOSCELES")
-                else:
-                    print("ES UN TRIANGULO ESCALENO")
+        if lado1+lado2>lado3 and lado1+lado3>lado2 and lado2+lado3>lado1:
+            if lado1==lado2 and lado1==lado3:
+                print("ES UN TRIANGULO EQUILATERO")
+            elif lado1==lado2 or lado1==lado3 or lado2==lado3:
+                print("ES UN TRIANGULO ISOSCELES")
             else:
-                print("NO ES TRIANGULO")
+                print("ES UN TRIANGULO ESCALENO")
+        else:
+            print("NO ES TRIANGULO")
     else:
         print("NO ES TRIANGULO")
 
